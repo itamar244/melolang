7cfe808d3566261900684cc1092685bfb066c689
itamar244 <itamary24@gmail.com>
2018-08-17 13:00:59 +0300

    default exports

--- include/melo/melo.h
+++ include/melo/melo.h
@@ -4,15 +4,22 @@
 #include <memory>
 #include "melo/ast.h"
 #include "melo/evaluator/module.h"
+#include "melo/evaluator/section_walker.h"
+#include "melo/evaluator/values.h"
 
 namespace melo {
 
+// exports for easy use
+using evaluator::Module;
+using evaluator::SectionWalker;
+using evaluator::PhraseValue;
+
 ast::BlockPtr Parse(std::istream& stream);
 
-inline evaluator::Module CreateModule(std::istream& stream) {
+inline Module CreateModule(std::istream& stream) {
 	return {Parse(stream)};
 }
-inline evaluator::Module CreateModule(ast::BlockPtr&& program) {
+inline Module CreateModule(ast::BlockPtr&& program) {
 	return {std::move(program)};
 }
 
